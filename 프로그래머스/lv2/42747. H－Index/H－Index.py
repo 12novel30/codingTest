c2ea2e47fce8bfd946fc3feb1186e172ab7c91fc
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-19 10:22:55 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,17 +1,8 @@
 def solution(citations):
     citations.sort()
-    count = []
-    tmpCount = 0
-    isAllZero = True
-    for i in range(len(citations)):
-        # [0,0,0,0,0] 예외 처리를 위함
-        if citations[i] != 0: isAllZero = False
-        
-        if i == 0 or citations[i] != citations[i-1]:
-            tmpCount = len(citations[i:])
-        count.append(tmpCount)
-        
-        if citations[i] >= count[i]:
-            return count[i]
     
-    if isAllZero == True: return 0
+    for i in range(len(citations)):
+        if citations[i] >= len(citations[i:]):
+            return len(citations[i:])
+    
+    return 0
