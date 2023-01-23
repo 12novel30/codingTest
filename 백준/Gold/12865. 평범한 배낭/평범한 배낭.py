5cc2fe263b56d6df60bcb49a050eb91bea5cdb7e
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-24 00:14:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 3272 ms, Memory: 228468 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,24 +1,27 @@
 # 0/1 knapsack
+def knapsack(num, maxWeight, weightList, valueList):
+    dp = [[0 for _ in range(maxWeight+1)] for _ in range(num+1)]
+    
+    for i in range(1, num + 1):
+        for j in range(1, maxWeight + 1):
+            if weightList[i-1] > j: # 지금 물건을 넣을 수 없다면
+                dp[i][j] = dp[i-1][j] # 이전 칸을 그대로
+            else: # 지금 물건을 넣을 수 있다면
+                dp[i][j] = max(dp[i-1][j],
+                               # 이전 칸을 그대로 or
+                               dp[i-1][j - weightList[i-1]] + valueList[i-1]
+                               # 이전 물건을 더하기 전
+                               # + 지금 물건의 value
+                               )
+    return dp[num][maxWeight]
 
-def knapsack(num,nump,numw,maxw):
-    dp=[[0 for _ in range(maxw+1)] for _ in range(num+1)]
-    for i in range(1, num+1):
-        for j in range(1, maxw+1):
-            if numw[i-1]>j:
-                dp[i][j]=dp[i-1][j]
-            else:
-                dp[i][j]=max(dp[i-1][j],
-                             dp[i-1][j-numw[i-1]] + nump[i-1])
-    return dp[num][maxw]
 
 # driver code
-num, maxweight=input().split()
-num=int(num)
-maxweight=int(maxweight)
-numprofit = [0]*(num)
-numweight = [0]*(num)
-for i in range(num):
-    numweight[i], numprofit[i]=input().split()
-    numweight[i] = int(numweight[i])
-    numprofit[i] = int(numprofit[i])
-print(knapsack(num,numprofit,numweight,maxweight))
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+wList = [0]*n
+vList = [0]*n
+for i in range(n):
+    wList[i],  vList[i] = map(int, sys.stdin.readline().split())
+print(knapsack(n, k, wList, vList))
