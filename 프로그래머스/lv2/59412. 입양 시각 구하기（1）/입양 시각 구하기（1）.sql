acda4cb0e1ed03d9c7ab887b2d76fcc9c74be5c6
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-24 23:38:35 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,12 @@
 -- 코드를 입력하세요
-SELECT date_format(DATETIME, '%k') as HOUR, count(ANIMAL_ID) as COUNT
+# SELECT date_format(DATETIME, '%k') as HOUR, count(ANIMAL_ID) as COUNT
+# from ANIMAL_OUTS
+# group by HOUR
+# having HOUR between 9 and 19 
+# order by date_format(DATETIME, '%H')
+
+SELECT extract(HOUR from DATETIME) as HOUR, count(ANIMAL_ID) as COUNT
 from ANIMAL_OUTS
-group by date_format(DATETIME, '%k')
+group by HOUR
 having HOUR between 9 and 19 
-order by date_format(DATETIME, '%H')
+order by HOUR
