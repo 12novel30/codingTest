7712690faf6e7942755d8989f41f5439b6e10261
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-24 23:16:57 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,8 +1,21 @@
+# 코테 준비중
 def solution(citations):
+    answer = 1
     citations.sort()
+    if citations[0] > len(citations): return len(citations) 
+    for c in reversed(citations):
+        if c >= answer: answer += 1
+        else: return answer - 1
     
-    for i in range(len(citations)):
-        if citations[i] >= len(citations[i:]):
-            return len(citations[i:])
-    
-    return 0
+    return answer
+
+'''
+0 1 2 5 6 -> 2
+5 4 3 2 1
+
+0 1 3 5 6 -> 3
+5 4 3 2 1
+
+0 1 4 5 6 -> 3
+5 4 3 2 1
+'''
