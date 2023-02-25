d1fade657b0caf915fce80d0a40164310f56b44b
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-25 11:57:23 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,14 +1,45 @@
 -- 코드를 입력하세요
-select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
-from MEMBER_PROFILE m left outer join REST_REVIEW r
-    on m.MEMBER_ID = r.MEMBER_ID
-where m.MEMBER_ID in (select MEMBER_ID
-                     from REST_REVIEW 
-                     group by MEMBER_ID
-                     having count(*) = (select count(*)
-                                       from REST_REVIEW
-                                       group by MEMBER_ID
-                                        order by count(*) desc
-                                       limit 1)
-                     )
+# select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
+# from MEMBER_PROFILE m left outer join REST_REVIEW r
+#     on m.MEMBER_ID = r.MEMBER_ID
+# where m.MEMBER_ID in (select MEMBER_ID
+#                      from REST_REVIEW 
+#                      group by MEMBER_ID
+#                      having count(*) = (select count(*)
+#                                        from REST_REVIEW
+#                                        group by MEMBER_ID
+#                                         order by count(*) desc
+#                                        limit 1)
+#                      )
+# order by REVIEW_DATE, REVIEW_TEXT
+
+
+
+
+
+
+
+
+
+
+# MEMBER_NAME, REVIEW_TEXT, REVIEW_DATE
+select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, "%Y-%m-%d") as REVIEW_DATE
+from member_profile p left outer join rest_review r
+on p.member_id = r.member_id
+where p.MEMBER_ID in (select MEMBER_ID
+                   from rest_review
+                   group by member_id
+                   having count(*) = (select count(*)
+                                              from rest_review
+                                              group by member_id
+                                                order by count(*) desc
+                                              limit 1)
+                   )
 order by REVIEW_DATE, REVIEW_TEXT
+
+
+
+
+
+
+
