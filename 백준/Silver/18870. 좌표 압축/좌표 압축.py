398c030bc3cd3cf553db685025bba37b6b4f07de
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-25 11:16:42 +0900

    [Silver II] Title: 좌표 압축, Time: 2084 ms, Memory: 154792 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,28 +1,6 @@
-# 코테 준비중
 n = int(input())
 xlist = list(map(int, input().split()))
-for i,x in enumerate(xlist):
-    xlist[i] = [x,i]
-xlist.sort()
-
-answer = []
-i = 0
-count = 0
-while i < len(xlist)-1:
-    hold = i
-    while xlist[i][0] == xlist[i+1][0]:
-        i += 1
-        if i == len(xlist)-1: break
-    if hold == i: answer.append([count, xlist[i][1]])
-    else:
-        for _ in range(hold, i+1):
-            answer.append([count, xlist[_][1]])
-    i += 1
-    count += 1
-
-if len(answer) != len(xlist):
-    answer.append([count, xlist[-1][1]])
-    
-answer = sorted(answer, key=lambda x: x[1])
-for a in answer:
-    print(a[0], end = ' ')
+xset = sorted(set(xlist))
+dict = {xset[i] : i for i in range(len(xset))}
+for d in xlist:
+    print(dict[d], end = ' ')
