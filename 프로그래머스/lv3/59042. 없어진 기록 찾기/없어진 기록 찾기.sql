57119f6750113709fef655689b012cc76898b9c0
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-02-25 11:38:57 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,36 @@
 -- 코드를 입력하세요
-SELECT o.ANIMAL_ID as ANIMAL_ID, o.NAME as NAME
-from ANIMAL_OUTS o left outer join ANIMAL_INS i
-    on o.ANIMAL_ID = i.ANIMAL_ID -- natural 쓰면 id 없는 애들까지 다 사라짐
-where i.ANIMAL_ID is null
-order by o.ANIMAL_ID
+# SELECT o.ANIMAL_ID as ANIMAL_ID, o.NAME as NAME
+# from ANIMAL_OUTS o left outer join ANIMAL_INS i
+#     on o.ANIMAL_ID = i.ANIMAL_ID -- natural 쓰면 id 없는 애들까지 다 사라짐
+# where i.ANIMAL_ID is null
+# order by o.ANIMAL_ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select o.ANIMAL_ID as ANIMAL_ID, o.NAME as NAME
+from animal_outs o left outer join animal_ins i 
+    on o.animal_id = i.animal_id
+where i.datetime is null
+
+
+
+
+
+
+
+
+
+
