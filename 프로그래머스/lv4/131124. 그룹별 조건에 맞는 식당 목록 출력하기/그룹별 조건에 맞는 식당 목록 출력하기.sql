db49625e492d8d6221b5a010c910faf213097f65
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-03-04 13:40:58 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -17,6 +17,27 @@
 
 
 
+# MEMBER_NAME, REVIEW_TEXT, REVIEW_DATE
+# select MEMBER_NAME, REVIEW_TEXT, date_format(REVIEW_DATE, "%Y-%m-%d") as REVIEW_DATE
+# from rest_review r left outer join member_profile p
+# on r.member_id = p.member_id
+# group by r.member_id
+# having r.member_id in (select member_id
+#                       from rest_review
+#                       group by member_id
+#                       having count(*) = (select count(*)
+#                                         from rest_review
+#                                         group by member_id
+#                                         order by count(*) desc
+#                                         limit 1))
+# order by REVIEW_DATE, REVIEW_TEXT
+
+
+
+
+
+
+
 
 
 
@@ -36,10 +57,3 @@ where p.MEMBER_ID in (select MEMBER_ID
                                               limit 1)
                    )
 order by REVIEW_DATE, REVIEW_TEXT
-
-
-
-
-
-
-
